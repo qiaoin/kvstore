ce84b37640c4fb16d398bcac90b93d5b82ec2045
qiao <qiao.liubing@gmail.com>
2021-08-29 14:38:46 +0800

    feat: project 2, part 1-6

--- src/kv.rs
+++ src/kv.rs
@@ -1,8 +1,10 @@
 use serde::{Deserialize, Serialize};
+use serde_json::Deserializer;
 use std::collections::HashMap;
+use std::ffi::OsStr;
 use std::fs::{self, File, OpenOptions};
 use std::io::{self, BufReader, BufWriter, Read, Seek, SeekFrom, Write};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use crate::{KvsError, Result};
 
@@ -39,9 +41,11 @@ impl Command {
 /// assert_eq!(store.get("key1".to_owned()), None);
 /// ```
 pub struct KvStore {
-    // path: PathBuf,
-    // reader of the current log
-    reader: BufferReaderWithPos<File>,
+    // directory for the log and other data.
+    path: PathBuf,
+    current_gen: u64,
+    // map generation number to the file reader.
+    readers: HashMap<u64, BufferReaderWithPos<File>>,
     // writer of the current log.
     writer: BufferWriterWithPos<File>,
     // an in-memory [key -> log pointer] map.
@@ -49,54 +53,84 @@ pub struct KvStore {
 }
 
 impl KvStore {
-    /// Open the KvStore at a given path. Return the KvStore.
+    /// Open the `KvStore` at a given path. Return the KvStore.
+    ///
+    /// This will create a new directory if the given dir does not exist.
+    ///
+    /// # Errors
+    ///
+    /// It propagates I/O or deserialilzation errors during the log re-play.
     pub fn open(path: impl Into<PathBuf>) -> Result<KvStore> {
-        let dir = path.into();
-        fs::create_dir_all(&dir)?;
-        let path = dir.join(format!("{}.log", 1123));
-        let reader = BufferReaderWithPos::new(
-            OpenOptions::new()
-            .create(true)
-            .write(true)
-            .read(true)
-            .open(&path)?, 
-        )?;
-        let writer = BufferWriterWithPos::new(
-            OpenOptions::new()
-                .create(true)
-                .write(true)
-                .append(true)
-                .open(&path)?,
-        )?;
+        let path = path.into();
+        fs::create_dir_all(&path)?;
+
+        let mut readers = HashMap::new();
+        let mut index = HashMap::new();
+
+        let gen_list = sorted_gen_list(&path)?;
+        for &gen in &gen_list {
+            let mut reader = BufferReaderWithPos::new(File::open(log_path(&path, gen))?)?;
+            load(gen, &mut reader, &mut index)?;
+            readers.insert(gen, reader);
+        }
+
+        let current_gen = gen_list.last().unwrap_or(&0) + 1;
+
+        let writer = new_log_file(&path, current_gen, &mut readers)?;
+
         Ok(KvStore {
-            // path
-            reader,
+            path,
+            current_gen,
+            readers,
             writer,
-            // TODO: 需要在 startup 的时候 load 一下
-            index: HashMap::new(),
+            index,
         })
     }
 
     /// Set the value of a string key to a string
+    ///
+    /// If the key already exists, the previous value will be overwritten.
+    ///
+    /// # Errors
+    ///
+    /// It propagates I/O or serialization errors during writing the log.
     pub fn set(&mut self, key: String, value: String) -> Result<()> {
         let cmd = Command::set(key, value);
+        let pos = self.writer.pos;
         serde_json::to_writer(&mut self.writer, &cmd)?;
         self.writer.flush()?;
+
+        if let Command::Set { key, value: _ } = cmd {
+            self.index.insert(
+                key,
+                CommandPos {
+                    gen: self.current_gen,
+                    start: pos,
+                    length: self.writer.pos - pos,
+                },
+            );
+        }
 
         Ok(())
     }
 
     /// Get the string value of the a string key.
     ///
-    /// If the key does not exist, return None.
+    /// If the key does not exist, return `None`.
+    ///
+    /// # Errors
+    ///
+    /// It returns `KvsError::UnexpectedCommandType` if the given command type unexpected.
     pub fn get(&mut self, key: String) -> Result<Option<String>> {
-        // reads the entire log, one command at a time, recording the affected key and
-        // file offset of the command to an in-memory [key -> log pointer] map
         if let Some(cmd_pos) = self.index.get(&key) {
+            let reader = self
+                .readers
+                .get_mut(&cmd_pos.gen)
+                .expect("Cannot find log reader");
             // key --> command's start postion
-            self.reader.seek(SeekFrom::Start(cmd_pos.start))?;
+            reader.seek(SeekFrom::Start(cmd_pos.start))?;
             // key --> command's length
-            let cmd_reader = self.reader.by_ref().take(cmd_pos.length);
+            let cmd_reader = reader.take(cmd_pos.length);
             if let Command::Set { key: _, value } = serde_json::from_reader(cmd_reader)? {
                 Ok(Some(value))
             } else {
@@ -108,15 +142,20 @@ impl KvStore {
     }
 
     /// Remove a given key.
+    ///
+    /// # Errors
+    ///
+    /// It returns `KvsError::KeyNotFound` if the given key is not found.
+    ///
+    /// It propagates I/O or serialization errors during writing the log.
     pub fn remove(&mut self, key: String) -> Result<()> {
         if self.index.contains_key(&key) {
             let cmd = Command::remove(key);
             serde_json::to_writer(&mut self.writer, &cmd)?;
             self.writer.flush()?;
 
-            // TODO: 从 index 中删除 key 对应的 entry
-            if let Command::Remove{ key } = cmd {
-                self.index.remove(&key).expect("key not found");
+            if let Command::Remove { key } = cmd {
+                self.index.remove(&key);
             }
 
             Ok(())
@@ -126,14 +165,92 @@ impl KvStore {
     }
 }
 
+/// Load the whole log file and store value locations in the index map.
+fn load(
+    gen: u64,
+    reader: &mut BufferReaderWithPos<File>,
+    index: &mut HashMap<String, CommandPos>,
+) -> Result<u64> {
+    //  make sure we read from the beginning of the file
+    let mut pos = reader.seek(SeekFrom::Start(0))?;
+    let mut stream = Deserializer::from_reader(reader).into_iter::<Command>();
+    while let Some(cmd) = stream.next() {
+        let next_pos = stream.byte_offset() as u64;
+        match cmd? {
+            Command::Set { key, value: _ } => {
+                index.insert(
+                    key,
+                    CommandPos {
+                        gen,
+                        start: pos,
+                        length: next_pos - pos,
+                    },
+                );
+            }
+            Command::Remove { key } => {
+                index.remove(&key);
+            }
+        }
+        pos = next_pos;
+    }
+    Ok(0)
+}
+
+/// Returns sorted generation numbers in the given directory.
+fn sorted_gen_list(path: &Path) -> Result<Vec<u64>> {
+    // TODO: 文件查找与遍历，这个有空就看一下
+    let mut gen_list: Vec<u64> = fs::read_dir(&path)?
+        .flat_map(|res| -> Result<_> { Ok(res?.path()) })
+        .filter(|path| path.is_file() && path.extension() == Some("log".as_ref()))
+        .flat_map(|path| {
+            path.file_name()
+                .and_then(OsStr::to_str)
+                .map(|s| s.trim_end_matches(".log"))
+                .map(str::parse::<u64>)
+        })
+        .flatten()
+        .collect();
+
+    gen_list.sort_unstable();
+    Ok(gen_list)
+}
+
+fn log_path(dir: &Path, gen: u64) -> PathBuf {
+    dir.join(format!("{}.log", gen))
+}
+
+/// Create a new log file with given generation number and add the reader to the readers map.
+///
+/// Returns the writer to the log.
+fn new_log_file(
+    path: &Path,
+    gen: u64,
+    readers: &mut HashMap<u64, BufferReaderWithPos<File>>,
+) -> Result<BufferWriterWithPos<File>> {
+    let path = log_path(path, gen);
+    let writer = BufferWriterWithPos::new(
+        OpenOptions::new()
+            .create(true)
+            .write(true)
+            .append(true)
+            .open(&path)?,
+    )?;
+    readers.insert(gen, BufferReaderWithPos::new(File::open(&path)?)?);
+
+    Ok(writer)
+}
+
+#[derive(Debug)]
+/// Represents the positon and length of a json-serialized command in the log.
+/// Include the command generation
 struct CommandPos {
+    gen: u64,
     start: u64,
     length: u64,
 }
 
 struct BufferWriterWithPos<W: Write + Seek> {
     writer: BufWriter<W>,
-    // TODO: writer 需要 pos 吗？
     pos: u64,
 }
 
@@ -150,6 +267,7 @@ impl<W: Write + Seek> BufferWriterWithPos<W> {
 impl<W: Write + Seek> Write for BufferWriterWithPos<W> {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
         let len = self.writer.write(buf)?;
+        self.pos += len as u64;
         Ok(len)
     }
 
@@ -184,6 +302,7 @@ impl<R: Read + Seek> BufferReaderWithPos<R> {
 impl<R: Read + Seek> Read for BufferReaderWithPos<R> {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         let len = self.reader.read(buf)?;
+        // self.pos += len as u64;
         Ok(len)
     }
 }
